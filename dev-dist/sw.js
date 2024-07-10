f082d4e90a36102d2a1d29883924975841949c81
hugogross <hugobombeirobbs@gmail.com>
2024-07-10 16:08:45 -0300

    dispositivos moveis2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qjo2jru6oqo"
+    "revision": "0.tl2a9p3m1eg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

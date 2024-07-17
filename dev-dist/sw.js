7b0cdca133a3742eecc1d05a1f014e09413369ff
hugogross <hugobombeirobbs@gmail.com>
2024-07-17 17:00:39 -0300

    nao sei

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7q4ddss6f8g"
+    "revision": "0.cfba6u7lm08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
